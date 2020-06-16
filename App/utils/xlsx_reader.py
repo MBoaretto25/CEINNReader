032b65ecd3dd3ce5dbb4831678a455a05cdf6229
Marco <marco.boaretto19@gmail.com>
2020-06-15 21:09:34 -0300

    move utils xlsx_reader to App package

--- App/utils/xlsx_reader.py
+++ App/utils/xlsx_reader.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from pandas.core.indexes.datetimes import DatetimeIndex
 
 
-def read_xlsl(file_path):
+def read_xls(file_path):
     """
     Read the xlsx files and return a dataframe with cleaned and parsed data
     :param file_path: path of the file
@@ -23,5 +24,20 @@ def read_xlsl(file_path):
     df = df.drop(columns=cols_to_drop)
     # Drop Last 4 Rows
     df = df.drop(df.index[-4:])
+
+    date_column = df.columns[0]
+    c_v_column = df.columns[1]
+
+    #stripping spaces in date columns
+    for index, row in df.iterrows():
+            row = row.copy()
+            df.loc[index, date_column] = row[0].strip()
+            df.loc[index, c_v_column] = row[1].strip()
+
+    df[date_column] = pd.to_datetime(df[date_column], format='%d/%m/%y')
+
+    df['Year'] = DatetimeIndex(df[date_column]).year
+    df['Month'] = DatetimeIndex(df[date_column]).month
+    df['Day'] = DatetimeIndex(df[date_column]).day
 
     return df
